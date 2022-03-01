a94d8ab78fcc243d85358dedab54f9bb4c77cd2b
Mori <dev@8bell.com>
2022-03-01 19:29:06 +0900

    Todo App v1.3.8

--- TODO second/src/App.js
+++ TODO second/src/App.js
@@ -3,13 +3,42 @@ import "./App.css"
 
 export class App extends Component {
 
-  btnstyle ={
+  //변하지 않는 스타일이므로 객체로 표현
+  btnstyle = {
     color: "#fff",
-    border:"none",
+    border: "none",
     padding: "5px 9px",
     borderRadius: "50%",
     cursor: "pointer",
     float: "right",
+  };
+
+  //동적으로 작동하는 스타일이므로 함수로 표현
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+
+  //반복적으로 생성되는 UI 요소를 map()으로 표현하기 위해 배열 사용
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ];
+ 
+  handleClick = (id) => {
+    let newTodoData = this.todoData.filter((data)=> data.id !== id);
+    this.todoData = newTodoData
   };
 
   render() {
@@ -19,11 +48,18 @@ export class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
-          <div>
-            <input type="checkbox" defaultChecked={false}/> 공부하기
-            <button style={this.btnstyle}>╳</button>
-          </div> 
 
+          {/* map() 메소드 사용해서 list 나열 */}
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <p>
+                <input type="checkbox" defaultChecked={false} />
+                {""}{data.title}
+                <button style={this.btnstyle} onClick={()=>this.handleClick(data.id)}>╳</button>
+              </p>
+            </div>
+          ))}
+ 
         </div>
       </div>
     )
@@ -31,4 +67,3 @@ export class App extends Component {
 }
 
 export default App
- 
