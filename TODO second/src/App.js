ceb127ba4134a1933350e58cae85d40cbb9ddbc7
Mori <dev@8bell.com>
2022-03-01 20:53:23 +0900

    Todo App v1.3.11

--- TODO second/src/App.js
+++ TODO second/src/App.js
@@ -21,30 +21,54 @@ export class App extends Component {
       textDecoration: "none",
     };
   };
-     // 랜더링을 위한 React state 생성 
-  state = {
-     //반복적으로 생성되는 UI 요소를 map()으로 표현하기 위해 배열 사용
-  todoData : [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true,
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false,
-    },
-  ],
 
+  // 랜더링을 위한 React state 생성 
+  state = {
+    //반복적으로 생성되는 UI 요소를 map()으로 표현하기 위해 배열 사용
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true,
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false,
+      },
+    ],
+    value: "",
   };
- 
- 
+
+  // x 누를 때 State에서 list 배열 업데이트 
   handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data)=> data.id !== id);
-    console.log('newTodoData',newTodoData)
-    this.setState({todoData : newTodoData}); // state 변경법 ***
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    this.setState({ todoData: newTodoData }); // state 변경법 ***
   };
+
+  // 입력할 때마다 State에서 value 값 e.target.value로 업데이트
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+
+    e.preventDefault(); // form의 value 값을 input 할 때 페이지 리로드 막아줌
+
+    // 새로운 할일 데이터 생성
+    let newTodo = {
+      id: Date.now,
+      title: this.state.value,
+      completed: false,
+    };
+
+    //원래 할 일에 새로운 데이터 더해주기 : 전개 연산자 DeepCopy
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+    
+    //submit 후 form의 value 값 비우기
+    this.setState({ value: "" });
+  };
+
 
   render() {
     return (
@@ -54,17 +78,36 @@ export class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          {/* map() 메소드 사용해서 list 나열 */}
+          {/* List 부분 : map() 메소드 사용해서 list 나열 */}
           {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <p>
                 <input type="checkbox" defaultChecked={false} />
                 {""}{data.title}
-                <button style={this.btnstyle} onClick={()=>this.handleClick(data.id)}>╳</button>
+                <button style={this.btnstyle} onClick={() => this.handleClick(data.id)}>╳</button>
               </p>
             </div>
           ))}
- 
+
+          {/* Form 부분 : 입력시 state 변경해야 입력한 값이 랜더링 됨*/}
+          <form style={{ display: "flex" }} >
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력 하세요."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value='입력'
+              className='btn'
+              style={{ flex: '1' }}
+              onClick={this.handleSubmit}
+            />
+          </form>
+
         </div>
       </div>
     )
